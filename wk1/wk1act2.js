a560056f220375a9442e09dab63ddde4e7f8ffa3
nikasparks <nikasparks@mac.com>
2024-04-27 23:06:07 -0600

    adding asignments

---
+++ wk1/wk1act2.js
@@ -0,0 +1,7 @@
+let myApples = 4;
+let friendApples = 2;
+let total = myApples + friendApples;
+
+document.querySelector('#myApples').innerHTML = myApples;
+document.querySelector("#friendApples").innerHTML = friendApples;
+document.querySelector("#total").innerHTML = total;
