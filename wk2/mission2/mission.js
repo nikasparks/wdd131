1ad617be23087836d88299fadf54bd259988ae99
nikasparks <nikasparks@mac.com>
2024-05-05 03:28:45 -0600

    fixed bug

--- wk2/mission2/mission.js
+++ wk2/mission2/mission.js
@@ -1,21 +1,20 @@
-const chooseTheme = document.querySelector("select, option");
-const imgLogo = document.querySelector(".BYU-logo");
-const bodyTheme = document.body
-
+const chooseTheme = document.querySelector("select");
+const imgLogo = document.querySelector(".BYUI-logo");
+console.log(imgLogo);
 function changeTheme(){
     let value = chooseTheme.value;
     console.log(value);
 
     if (value === 'dark'){
         document.body.classList.add('dark');
-        // imgLogo.setAttribute('src', 'images/byui-logo_white.png');
-        // imgLogo.setAttribute('alt', 'BYU-I logo in white');
+        imgLogo.setAttribute('src', 'images/byui-logo_white.png');
+        imgLogo.setAttribute('alt', 'BYU-I logo in white');
     }else{
         document.body.classList.remove('dark');
-        // imgLogo.setAttribute('src', 'images/byui-logo_blue.webp');
-        // imgLogo.setAttribute('alt', 'BYU-I Logo');
+        imgLogo.setAttribute('src', 'images/byui-logo_blue.webp');
+        imgLogo.setAttribute('alt', 'BYU-I Logo');
     }
 }
 
 
-chooseTheme.addEventListener('click', changeTheme);
+chooseTheme.addEventListener('change', changeTheme);
