a249f23c99f28ae68580d49b7d742fd845128e9d
nikasparks <nikasparks@mac.com>
2024-05-21 21:24:39 -0600

    s

--- unit2/wk3/coolpics/coolpics.js
+++ unit2/wk3/coolpics/coolpics.js
@@ -27,14 +27,23 @@ function handleResize() {
 
   function viewHandler(event) {
 	// create a variable to hold the element that was clicked on from event.target
-    const picName = event.target
+    const picInfo = event.target;
 	// get the src attribute from that element and 'split' it on the "-"
+    const picSorce = picInfo.src.split("-")
 
+    console.log(picSorce)
+    const picName = picSorce[0]
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const picNew = `"${picName}-full.jpeg"`
 
+    console.log(picNew)
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
-
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(picNew,'New pic'));
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    const closeButton = document.querySelector('.close-viewer');
+    // closeButton.addEventListener("click", closeViewer);
 
 }
+
+document.querySelector(".gallery").addEventListener("click", viewHandler);
