13077aab4febdd2b52837cef8859318615f3d19b
nikasparks <nikasparks@mac.com>
2024-06-30 19:35:42 -0600

    d

--- unit3/wk6/blog/blog.js
+++ unit3/wk6/blog/blog.js
@@ -91,19 +91,3 @@ function createArticle(articles){
 }
 
 createArticle(articles)
-// const newParagraph = document.createElement("p");
-// newParagraph.innerText = "Added with Javascript!";
-// document.body.appendChild(newParagraph);
-
-// const newImg = document.createElement("img");
-// newImg.setAttribute("src", "https://picsum.photos/200");
-// newImg.setAttribute("alt", "Random image.");
-// document.body.appendChild(newImg);
-
-// const newDiv = document.createElement("div");
-// newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
-// document.body.appendChild(newDiv);
-
-// const newSec = document.createElement("section");
-// newSec.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
-// document.body.appendChild(newSec);
--- unit4/wk8/register/register.js
+++ unit4/wk8/register/register.js
@@ -1,9 +1,65 @@
-const newParticipant = document.querySelector('#add');
+const newParticipantBtn = document.querySelector('#add');
 let count = 1;
 
 function participantTemplate(count){
+    //create new elements
+    const newParticipant = document.createElement("section");
+    const newTitle = document.createElement("p");
+    const nameDiv = document.createElement("div");
+    const nameLabel = document.createElement("label");
+    const nameInput = document.createElement("input");
+    const actDiv = document.createElement("div");
+    const actLabel = document.createElement("label");
+    const actInput = document.createElement("input");
+    const feeDiv = document.createElement("div");
+    const feeLabel = document.createElement("label");
+    const feeInput = document.createElement("input");
+    const dateDiv = document.createElement("div");
+    const dateLabel = document.createElement("label");
+    const dateInput = document.createElement("input");
+    const gradeDiv = document.createElement("div");
+    const gradeTitle = document.createElement("p");
+    const gradeSelect = document.createElement("select");
 
-    const newParticipantSec = document.createElement("section"); g
+    //adding classes and ids
+    newParticipant.classList
+    //adding content
+
+    //adding it to the HTML
+    nameDiv.appendChild(nameLabel);
+    nameDiv.appendChild(nameInput);
+    actDiv.appendChild(actLabel);
+    actDiv.appendChild(actInput);
+    feeDiv.appendChild(feeLabel);
+    feeDiv.appendChild(feeInput);
+    dateDiv.appendChild(dateLabel);
+    dateDiv.appendChild(dateInput);
+    gradeDiv.appendChild(gradeTitle);
+    gradeDiv.appendChild(gradeSelect);
+    newParticipant.appendChild(newTitle);
+    newParticipant.appendChild(nameDiv);
+    newParticipant.appendChild(actDiv);
+    newParticipant.appendChild(feeDiv);
+    newParticipant.appendChild(dateDiv);
+    newParticipant.appendChild(gradeDiv);
+
+
+// const newParagraph = document.createElement("p");
+// newParagraph.innerText = "Added with Javascript!";
+// document.body.appendChild(newParagraph);
+
+// const newImg = document.createElement("img");
+// newImg.setAttribute("src", "https://picsum.photos/200");
+// newImg.setAttribute("alt", "Random image.");
+// document.body.appendChild(newImg);
+
+// const newDiv = document.createElement("div");
+// newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+// document.body.appendChild(newDiv);
+
+// const newSec = document.createElement("section");
+// newSec.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+// document.body.appendChild(newSec);
     // <section class="participant1">
     //         <p>Participant 1</p>
     //         <div class="item">
@@ -48,4 +104,4 @@ function participantTemplate(count){
 
 };
 
-newParticipant.addEventListener('click',participantTemplate());
+newParticipantBtn.addEventListener('click',participantTemplate());
